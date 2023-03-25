c2c3997e09f87b8ee20e7d616ec1608a22981dad
lilyarya <91736494+lilyarya@users.noreply.github.com>
2023-03-25 18:38:25 +0530

    My first commit including routes controllers till layouts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,8 @@
 module.exports.home = function(req,res){
-    return res.end('<h1> Express is up for Arial</h1>');
+    //return res.end('<h1> Express is up for Arial</h1>');'
+    // returning directly to browser
+
+    return res.render('home',{
+        title : "Home"
+    });
 };
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express= require('express');      // not to be overidden so const
 const app = express();
 const port = 8000;
-//use express router
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'))
+
+app.use(expressLayouts);
+
+//use express router
 app.use('/',require('./routes/index')); // you can do only routes
+
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if (err){
