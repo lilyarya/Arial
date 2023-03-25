d17cadb0daeb251c4330789d4a0a9dfbc113c898
lilyarya <91736494+lilyarya@users.noreply.github.com>
2023-03-25 17:10:39 +0530

    Setiing up more orutes

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const homeController = require('../controllers/home_controller');
 //console.log('Loaded');  // to check
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 module.exports = router;
